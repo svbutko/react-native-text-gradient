fab84b43acfd1e10ef3d82901c39411c19dc7546
Sergei Butko <svbutko@hotmail.com>
2017-12-12 22:52:14 +0200

    Added types definition file

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+declare module "react-native-text-gradient" {
+    import { Component } from 'react'
+
+    interface TextGradientProps {
+        text: string;
+        style?: {
+            fontFamily?: string;
+            fontSize?: number;
+        }
+        colors: string[],
+        startPoint?: { x: number, y: number },
+        endPoint?: { x: number, y: number },
+        locations?: number[]
+    }
+
+    export class TextGradient extends Component<TextGradientProps> {}
+}
