c0c26ecc4db29ead3771fbd13d1947b1b477f330
svbutko <svbutko@hotmail.com>
2017-12-13 00:01:15 +0200

    Added and linked an example for Android

--- example/App.js
+++ example/App.js
@@ -4,8 +4,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, requireNativeComponent, StyleSheet, Text, View} from 'react-native';
+import React, {Component} from "react";
+import {Platform, requireNativeComponent, StyleSheet, Text, View} from "react-native";
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' +
@@ -14,15 +14,26 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-const Gradient = requireNativeComponent("SVBTextGradient", null);
-
-export default class App extends Component<{}> {
-    render() {
-        return (
-            <View style={styles.container}>
+const TextGradient = Platform.OS == "ios" ? requireNativeComponent("SVBTextGradient", null) : require("react-native-text-gradient");
+export default class App extends Component {
+    showTextGradient = (): JSX.Element | null => {
+        if (Platform.OS == "ios") {
+            return (
                 <View>
                     <Gradient style={{width: 100, height: 50, backgroundColor: "transparent"}}/>
                 </View>
+            );
+        } else {
+            TextGradient.show("RN-text-gradient has been linked successfully!", TextGradient.LONG);
+
+            return null;
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {this.showTextGradient()}
                 <Text style={styles.welcome}>
                     Welcome to React Native!
                 </Text>
@@ -42,16 +53,16 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#F5FCFF'
     },
     welcome: {
         fontSize: 20,
         textAlign: 'center',
-        margin: 10,
+        margin: 10
     },
     instructions: {
         textAlign: 'center',
         color: '#333333',
-        marginBottom: 5,
-    },
+        marginBottom: 5
+    }
 });
