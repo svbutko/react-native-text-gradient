76361338e686fac463c11cff8a0d0f4c494af55f
foerra <foerra@gmail.com>
2017-12-15 13:34:28 +0200

    update typings

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,18 @@
 declare module "react-native-text-gradient" {
-    import { Component } from "react";
+    import {Component} from "react";
+    import {ViewStyle} from "react-native";
 
     interface TextGradientProps {
+        style: ViewStyle;
         text: string;
         fontFamily?: string;
         fontSize?: number;
-        colors: string[],
-        startPoint?: { x: number, y: number },
-        endPoint?: { x: number, y: number },
-        locations?: number[]
+        colors: string[];
+        startPoint?: { x: number, y: number };
+        endPoint?: { x: number, y: number };
+        locations?: number[];
     }
 
-    export class TextGradient extends Component<TextGradientProps> {}
+    export class TextGradient extends Component<TextGradientProps> {
+    }
 }
