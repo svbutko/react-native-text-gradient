bd925ebe07c69583ab1a6f75590e0657cafd6d74
Igor Antsiferov <foerra@gmail.com>
2017-12-21 11:06:37 +0200

    align gradient to center

--- ios/SVBTextGradient/SVBTextGradient/SVBTextGradient.m
+++ ios/SVBTextGradient/SVBTextGradient/SVBTextGradient.m
@@ -32,6 +32,7 @@
     [super layoutSubviews];
 
     [_label sizeToFit];
+    _label.frame = CGRectMake(0, (self.frame.size.height - _label.frame.size.height) / 2.0, _label.frame.size.width, _label.frame.size.height);
     _gradient.frame = _label.frame;
 }
 
